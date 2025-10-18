b4c464a39eb796fd8a69bb6784f23b8ff2b3874b
Shashwat Samadhiya <sshashwat934@gmail.com>
2025-10-19 01:15:26 +0530

    changes

--- test_driver/integration_test.dart
+++ test_driver/integration_test.dart
@@ -1,8 +1,6 @@
 import 'package:integration_test/integration_test_driver.dart';
 import 'dart:io';
 
-///flutter drive --driver=test_driver/integration_test.dart --target=integration_test/app_test.dart --dart-define=WEATHER_API_ACCESS_KEY=db21f0e6d460f2fccbbb84651e0ee2e2 --dart-define=GOOGLE_MAPS_KEY=AIzaSyCbJC9DBHfHdsmfYyzo2r0LQEIKJQHgcT4
-
 Future<void> main() async {
   await Process.run('adb', [
     'shell',
